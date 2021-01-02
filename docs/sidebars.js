09f56a5684a5b2dbe6d70efe7e5a7faeb28c9876
趙子賢 <danny900714@gmail.com>
2021-01-02 21:45:29 +0800

    Change doc structure

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -1,7 +1,8 @@
 module.exports = {
   someSidebar: {
+    "介紹": ['changelog'],
     Kubernetes: ['kubernetes/overview', 'kubernetes/installation'],
-    Docusaurus: ['doc1', 'doc2', 'doc3'],
-    Features: ['mdx'],
+    /* Docusaurus: ['doc1', 'doc2', 'doc3'],
+    Features: ['mdx'], */
   },
 };
