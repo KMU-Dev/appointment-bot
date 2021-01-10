b2a30f8fd63970a151c3bb4e4441717b9e3989d7
趙子賢 <danny900714@gmail.com>
2021-01-10 20:15:48 +0800

    Suppress no-explicit-any warning

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { Action, chain, Context } from 'bottender';
 import RuleBasedRouter from './routers/RuleBasedRouter';
 
+// Temporary disable no-explicit-any warnings, possible fix: declare property interface
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const App: Action<Context, Record<string, any>> = async () => {
     return chain([RuleBasedRouter]);
 };
