0584e56603abb579e06c1437142e14d2504f8c9b
趙子賢 <danny900714@gmail.com>
2021-01-10 23:28:32 +0800

    完成 Node.js 內容

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
     mainSidebar: {
         介紹: ['changelog'],
-        'Node.js': ['nodejs/overview', 'nodejs/installation'],
+        'Node.js': ['nodejs/overview', 'nodejs/installation', 'nodejs/tools'],
         Kubernetes: ['kubernetes/overview', 'kubernetes/installation', 'kubernetes/tools'],
         附錄: ['appendix/chocolatey'],
         /* Docusaurus: ['doc1', 'doc2', 'doc3'],
