fbb4d60fcb696aec03f674c9eb2bbb17c76c3522
趙子賢 <danny900714@gmail.com>
2021-01-14 12:46:41 +0800

    Add catalogue and typescript overview

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -1,7 +1,8 @@
 module.exports = {
     mainSidebar: {
-        介紹: ['introduction/welcome', 'introduction/schedule', 'introduction/changelog'],
+        介紹: ['introduction/welcome', 'introduction/schedule', 'introduction/catalogue', 'introduction/changelog'],
         'Node.js': ['nodejs/overview', 'nodejs/installation', 'nodejs/tools'],
+        'TypeScript': ['typescript/overview'],
         Kubernetes: ['kubernetes/overview', 'kubernetes/installation', 'kubernetes/tools'],
         附錄: ['appendix/chocolatey'],
         /* Docusaurus: ['doc1', 'doc2', 'doc3'],
