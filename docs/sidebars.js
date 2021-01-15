3c05f26c9bc65cd4749112a716cbd3b04bfea5e6
趙子賢 <danny900714@gmail.com>
2021-01-15 22:32:19 +0800

    Add TypeScript first-project and change tsconfig.json settings

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -2,7 +2,7 @@ module.exports = {
     mainSidebar: {
         介紹: ['introduction/welcome', 'introduction/schedule', 'introduction/catalogue', 'introduction/changelog'],
         'Node.js': ['nodejs/overview', 'nodejs/installation', 'nodejs/tools'],
-        'TypeScript': ['typescript/overview'],
+        'TypeScript': ['typescript/overview', 'typescript/first-project'],
         Kubernetes: ['kubernetes/overview', 'kubernetes/installation', 'kubernetes/tools'],
         附錄: ['appendix/chocolatey'],
         /* Docusaurus: ['doc1', 'doc2', 'doc3'],
