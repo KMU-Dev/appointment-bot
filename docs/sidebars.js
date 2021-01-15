d9d378249de5fd3c62917a3715fb030357ea5112
趙子賢 <danny900714@gmail.com>
2021-01-16 00:44:07 +0800

    Add variable and comment tutorial

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -2,7 +2,7 @@ module.exports = {
     mainSidebar: {
         介紹: ['introduction/welcome', 'introduction/schedule', 'introduction/catalogue', 'introduction/changelog'],
         'Node.js': ['nodejs/overview', 'nodejs/installation', 'nodejs/tools'],
-        'TypeScript': ['typescript/overview', 'typescript/first-project'],
+        'TypeScript': ['typescript/overview', 'typescript/first-project', 'typescript/variable-and-comment'],
         Kubernetes: ['kubernetes/overview', 'kubernetes/installation', 'kubernetes/tools'],
         附錄: ['appendix/chocolatey'],
         /* Docusaurus: ['doc1', 'doc2', 'doc3'],
