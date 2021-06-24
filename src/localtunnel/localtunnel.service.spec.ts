609a1c0e45410df89bd7df56ec276c5ab2c1d6d6
趙子賢 <danny900714@gmail.com>
2021-06-25 01:13:25 +0800

    Fix test failed

--- src/localtunnel/localtunnel.service.spec.ts
+++ src/localtunnel/localtunnel.service.spec.ts
@@ -6,13 +6,23 @@ describe('NgrokService', () => {
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [LocaltunnelService],
+            providers: [
+                {
+                    provide: 'LOCALTUNNEL_PORT',
+                    useValue: 3000,
+                },
+                {
+                    provide: 'LOCALTUNNEL_SUBDOMAIN',
+                    useValue: '',
+                },
+                LocaltunnelService,
+            ],
         }).compile();
 
         service = module.get<LocaltunnelService>(LocaltunnelService);
     });
 
-    it('should be defined', () => {
+    it('should be defined when LOCALTUNNEL_PORT and LOCALTUNNEL_SUBDOMAIN is provided', () => {
         expect(service).toBeDefined();
     });
 });
